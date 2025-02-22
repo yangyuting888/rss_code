e9525c2097efb743a9954a0c01309dcf53432647
yangyuting888 <568396827@qq.com>
2025-02-22 15:54:32 +0800

    Add files via upload

---
+++ result_draw.m
@@ -0,0 +1,75 @@
+% % figure;
+% % hold on
+% %  load('dpj1227.mat');
+% %  load('zsy1227.mat');
+% %  load('zzx1227.mat');
+% %  load('dl1227.mat');
+% %  load("yyt1227.mat");
+% %  load('lhy1227.mat');
+% %  load("therods.mat")
+% %  dpj=flip(dpj);
+% %  zsy=flip(zsy);
+% %  zzx=flip(zzx);
+% %  dl=flip(dl);
+% %  yyt=flip(yyt);
+% %  lhy=flip(lhy);
+% %  plot(dpj(:,1:end-1),'-d','color',[255 187 0]/255,'LineWidth', 1.5,'MarkerFaceColor',[255 187 0]/255);
+% %  plot(zsy(:,1:end-1),'-^','color',[124 187 0]/255,'LineWidth', 1.5,'MarkerFaceColor',[124 187 0]/255);
+% %  plot(zzx(:,1:end-1),'-o','color',[0 0.45 0.74],'LineWidth', 1.5,'MarkerFaceColor',[0 0.45 0.74]);
+% %  plot(dl(:,1:end-1),'-*','color',[0.85 0.33 0.10],'LineWidth', 1.5,'MarkerFaceColor',[0 0.45 0.74]);
+% %  plot(yyt(:,1:end-1),'-x','color',[0 157 178]/255,'LineWidth', 1.5,'MarkerFaceColor',[0.85 0.33 0.10]);
+% %  plot(lhy(:,1:end-1),'-+','color',[0.49 0.18 0.56],'LineWidth', 1.5,'MarkerFaceColor',[0.49 0.18 0.56]);
+% % %   plot(flip(dpj(:,1:end)),'-d','color',[255 187 0]/255,'LineWidth', 1.5);
+% % %  plot(flip(zsy(:,1:end)),'-^','color',[124 187 0]/255,'LineWidth', 1.5);
+% % %  plot(flip(zzx(:,1:end)),'-+','color',[0 0.45 0.74],'LineWidth', 1.5);
+% % %  plot(flip(dl(:,1:end)),'-*','color',[0.85 0.33 0.10],'LineWidth', 1.5);
+% % %  plot(flip(yyt(:,1:end)),'-x','color',[0.93 0.69 0.13],'LineWidth', 1.5);
+% % %  plot(flip(lhy(:,1:end)),'-o','color',[0.49 0.18 0.56],'LineWidth', 1.5);
+% %  
+% %  plot(therods(:,1:end-1),'--','color',[246 83 20]/255,'LineWidth', 1.5)
+% %  %xticklabels({'9','8','7','6','5','4','3','2','1'});
+% %  xlabel('The distance between intruder and transceivers (m)');
+% %  ylabel('Power sum ratio');
+% %  set(gcf, 'Color', 'white');
+% %  set(gca, 'Color', 'white');
+% %  box(gca,'on');
+% %  legend({'volunteer1', 'volunteer2','volunteer3', 'volunteer4','volunteer5', 'volunteer6'}, 'location', 'northeast');
+% 
+% 
+% % figure;
+% % hold on
+% %  load('yyt0.mat');
+% %  load('yyt30.mat');
+% %  load('yyt60.mat');
+% %  load('yyt90.mat');
+% %  load("yyt180.mat");
+% %  load("therods.mat")
+% %  yyt0=flip(yyt0);
+% %  yyt30=flip(yyt30);
+% %  yyt60=flip(yyt60);
+% %  yyt90=flip(yyt90);
+% %  yyt180=flip(yyt180);
+% % 
+% %  plot(yyt0(:,1:end-1),'-d','color',[255 187 0]/255,'LineWidth', 1.5,'MarkerFaceColor',[255 187 0]/255);
+% %  plot(yyt30(:,1:end-1),'-^','color',[124 187 0]/255,'LineWidth', 1.5,'MarkerFaceColor',[124 187 0]/255);
+% %  plot(yyt60(:,1:end-1),'-o','color',[0 0.45 0.74],'LineWidth', 1.5,'MarkerFaceColor',[0 0.45 0.74]);
+% %  plot(yyt90(:,1:end-1),'-*','color',[0.85 0.33 0.10],'LineWidth', 1.5,'MarkerFaceColor',[0 0.45 0.74]);
+% %  plot(yyt180(:,1:end-1),'-x','color',[0 157 178]/255,'LineWidth', 1.5,'MarkerFaceColor',[0.85 0.33 0.10]);
+% % %plot(flip(lhy(:,1:end-1)),'-+','color',[0.49 0.18 0.56],'LineWidth', 1.5,'MarkerFaceColor',[0.49 0.18 0.56]);
+% %  
+% %  plot(therods(:,1:end-1),'--','color',[246 83 20]/255,'LineWidth', 1.5)
+% %  %xticklabels({'9','8','7','6','5','4','3','2','1'});
+% %  xlabel('The distance between intruder and transceivers (m)');
+% %  ylabel('Power sum ratio');
+% %  set(gcf, 'Color', 'white');
+% %  set(gca, 'Color', 'white');
+% %  box(gca,'on');
+% %  legend({'angle=0', 'angle=30','angle=60', 'angle=90','angle=180'}, 'location', 'northeast');
+
+
+
+
+
+
+
+
